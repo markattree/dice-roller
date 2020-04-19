4e98b4aa528c8264749d3d244df24766a099fc44
Mark Attree <markattree@hotmail.com>
2020-04-19 15:16:36 +0100

    Added ability to select a quantity of dice to roll between 1 and 10

--- static/js/script.js
+++ static/js/script.js
@@ -1,42 +1,56 @@
 window.onload = () => {
   document.getElementById("d2").addEventListener("click", (_) => {
-    rollDice(2)
+    let quantity = document.getElementById("d2-quantity").value
+    rollDice(2, quantity)
   });
   document.getElementById("d4").addEventListener("click", (_) => {
-    rollDice(4)
+    let quantity = document.getElementById("d4-quantity").value
+    rollDice(4, quantity)
   });
   document.getElementById("d6").addEventListener("click", (_) => {
-    rollDice(6)
+    let quantity = document.getElementById("d6-quantity").value
+    rollDice(6, quantity)
   });
   document.getElementById("d8").addEventListener("click", (_) => {
-    rollDice(8)
+    let quantity = document.getElementById("d8-quantity").value
+    rollDice(8, quantity)
   });
   document.getElementById("d10").addEventListener("click", (_) => {
-    rollDice(10)
+    let quantity = document.getElementById("d10-quantity").value
+    rollDice(10, quantity)
   });
   document.getElementById("d12").addEventListener("click", (_) => {
-    rollDice(12)
+    let quantity = document.getElementById("d12-quantity").value
+    rollDice(12, quantity)
   });
   document.getElementById("d20").addEventListener("click", (_) => {
-    rollDice(20)
+    let quantity = document.getElementById("d20-quantity").value
+    rollDice(20, quantity)
   });
   document.getElementById("d100").addEventListener("click", (_) => {
-    rollDice(100)
+    let quantity = document.getElementById("d100-quantity").value
+    rollDice(100, quantity)
   });
 }
 
-function rollDice(numberOfFaces) {
+function rollDice(numberOfFaces, numberOfDice) {
   updateResult("--")
-  return axios.get(`https://www.random.org/integers/?num=10&min=1&max=${numberOfFaces}&col=1&base=10&format=plain&rnd=new`)
+  updateRolled("--")
+  return axios.get(`https://www.random.org/integers/?num=${numberOfDice}&min=1&max=${numberOfFaces}&col=1&base=10&format=plain&rnd=new`)
     .then(response => {
-      let values = response.data.split("\n");
-      values.pop();
-      let rolled = values.join(" ");
-      let result = values.reduce((a, b) => {
-        return parseInt(a) + parseInt(b)
-      }, 0)
-      updateRolled(rolled)
-      updateResult(result);
+      if (numberOfDice > 1) {
+        let values = response.data.split("\n");
+        values.pop();
+        let rolled = values.join(" ");
+        let result = values.reduce((a, b) => {
+          return parseInt(a) + parseInt(b)
+        }, 0)
+        updateRolled(rolled)
+        updateResult(result);
+      } else {
+        updateResult(response.data)
+        updateRolled(response.data)
+      }
     })
     .catch(error => {
       console.error(error);
